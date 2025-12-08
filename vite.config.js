6ec2c2d96b6b45711f6126e241a049d0ae23c365
kailashrwt <krwt2277@gmail.com>
2025-12-09 00:11:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from "path";
 
 export default defineConfig({
+  base: "/",  
   plugins: [react()],
-  base: "/",   // ← यही सही है Render के लिए
-});
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  }
+})
