062e903cf21e893078c4483c35c17847a13d7f9e
kailashrwt <krwt2277@gmail.com>
2025-12-08 21:57:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/",   // 👈 सिर्फ ये रखना है
   plugins: [react()],
-  build: { outDir: "dist" }
 })
