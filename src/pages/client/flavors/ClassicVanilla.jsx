0b430c1adcd7355360c85525e99a7dfed2b0d005
kailashrwt <krwt2277@gmail.com>
2025-12-08 20:33:46 +0530

    Update ClassicVanilla.jsx

--- src/pages/client/flavors/ClassicVanilla.jsx
+++ src/pages/client/flavors/ClassicVanilla.jsx
@@ -13,7 +13,7 @@ const ClassicVanilla = ({ theme = "light" }) => {
         const fetchVanilla = async () => {
             try {
                 const res = await axios.get(
-                    "http://localhost:6060/api/products/category/Classic Vanilla"
+                    "https://ecommerce-backend-s1l7.onrender.com/api/products/category/Classic Vanilla"
                 );
 
                 if (res.data.success) {
@@ -67,7 +67,7 @@ const ClassicVanilla = ({ theme = "light" }) => {
                         >
                             {/* Product Image */}
                             <img
-                                src={`http://localhost:6060${item.image}`}
+                                src={`https://ecommerce-backend-s1l7.onrender.com${item.image}`}
                                 alt={item.name}
                                 className="w-full h-48 sm:h-56 md:h-60 object-cover rounded-lg mb-4"
                             />
