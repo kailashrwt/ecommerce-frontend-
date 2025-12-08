9132147e58515bcff70abfc54cf7fe8cd6ed0651
kailashrwt <krwt2277@gmail.com>
2025-12-08 21:44:47 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base : process.env.NODE_ENV === "production"
-  ?  '/E-commerce_project/'
-  : "/",
-  plugins: [react()]
+  plugins: [react()],
+  build: { outDir: "dist" }
 })
