4dfe79890b9cd772190134553d179afda03a335e
kailashrwt <krwt2277@gmail.com>
2025-12-08 20:42:57 +0530

    Update Hoops.jsx

--- src/pages/client/shop/Hoops.jsx
+++ src/pages/client/shop/Hoops.jsx
@@ -13,7 +13,7 @@ const Hoops = ({ theme = "light" }) => {
     const fetchHoops = async () => {
       try {
         const res = await axios.get(
-          "http://localhost:6060/api/products/category/Hoops"
+          "https://ecommerce-backend-s1l7.onrender.com/api/products/category/Hoops"
         );
 
         if (res.data.success) {
@@ -67,7 +67,7 @@ const Hoops = ({ theme = "light" }) => {
             >
               {/* Image */}
               <img
-                src={`http://localhost:6060${item.image}`}
+                src={`https://ecommerce-backend-s1l7.onrender.com${item.image}`}
                 alt={item.name}
                 className="w-full h-48 sm:h-56 md:h-60 object-cover rounded-lg mb-4"
               />
