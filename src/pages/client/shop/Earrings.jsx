845746c3cdf85697ebb028b83968a3eb5f00dccf
kailashrwt <krwt2277@gmail.com>
2025-12-08 20:42:26 +0530

    Update Earrings.jsx

--- src/pages/client/shop/Earrings.jsx
+++ src/pages/client/shop/Earrings.jsx
@@ -13,7 +13,7 @@ const Earrings = ({ theme = "light" }) => {
     const fetchEarrings = async () => {
       try {
         const res = await axios.get(
-          "http://localhost:6060/api/products/category/Earrings"
+          "https://ecommerce-backend-s1l7.onrender.com/api/products/category/Earrings"
         );
 
         if (res.data.success) {
@@ -67,7 +67,7 @@ const Earrings = ({ theme = "light" }) => {
             >
               {/* Image */}
               <img
-                src={`http://localhost:6060${item.image}`}
+                src={`https://ecommerce-backend-s1l7.onrender.com${item.image}`}
                 alt={item.name}
                 className="w-full h-48 sm:h-56 md:h-60 object-cover rounded-lg mb-4"
               />
