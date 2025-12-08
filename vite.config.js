73080e8a27e1e4df97e06ba240394dd9d0ad5d14
kailashrwt <krwt2277@gmail.com>
2025-12-08 22:42:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/",   // FINAL — Only slash, nothing else
+  base: "/",   
   plugins: [react()],
 })
