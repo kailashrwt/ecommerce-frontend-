dbccfe56ed28a23452b858780d94646fdefcf679
kailashrwt <krwt2277@gmail.com>
2025-12-18 15:28:34 +0530

    Update Bracelets.jsx

--- src/pages/client/shop/Bracelets.jsx
+++ src/pages/client/shop/Bracelets.jsx
@@ -9,6 +9,10 @@ const Bracelets = ({ theme = "light" }) => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+  const getImageUrl = (image) => {
+  if (!image) return "/placeholder.png";
+  return image; // Cloudinary full URL
+  };
 
   useEffect(() => {
     const fetchBracelets = async () => {
@@ -66,11 +70,7 @@ const Bracelets = ({ theme = "light" }) => {
               }}
             >
               {/* Image */}
-              <img
-                src={`https://ecommerce-backend-s1l7.onrender.com${item.image}`}
-                alt={item.name}
-                className="w-full h-48 sm:h-56 md:h-60 object-cover rounded-lg mb-4"
-              />
+              <img src={getImageUrl(item.image)} alt={item.name} />
 
               {/* Name */}
               <h2 className="text-lg sm:text-xl font-bold">{item.name}</h2>
